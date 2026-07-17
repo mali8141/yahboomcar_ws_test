e489647469507968b3bfa2ac3ea1fb8f887de128
mali8141 <malipawan.c.555@gmail.com.com>
2026-07-17 14:23:43 +0200

    update planer_move

---
+++ yahboomcar_ws/install/yahboom_M3Pro_description/share/yahboom_M3Pro_description/launch/gazebo_display_planar_move.launch.py
@@ -0,0 +1,48 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, TimerAction, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('yahboom_M3Pro_description')
+
+    share_parent = os.path.dirname(pkg_path)
+    existing_model_path = os.environ.get('GAZEBO_MODEL_PATH', '')
+    set_gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=share_parent + (':' + existing_model_path if existing_model_path else '')
+    )
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(pkg_path, 'launch', 'rsp.launch.py')
+        ]),
+        launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = ExecuteProcess(
+        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+        output='screen'
+    )
+
+    spawn_entity = TimerAction(
+        period=3.0,
+        actions=[
+            Node(
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                arguments=['-topic', 'robot_description', '-entity', 'M3Pro', '-z', '0.05'],
+                output='screen'
+            )
+        ]
+    )
+
+    return LaunchDescription([
+        set_gazebo_model_path,
+        rsp,
+        gazebo,
+        spawn_entity
+    ])
