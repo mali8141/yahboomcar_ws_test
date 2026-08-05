ccf8b34a5bb8ef3087278d2be759e3ec211e2321
mali8141 <malipawan.c.555@gmail.com.com>
2026-08-05 14:04:08 +0200

    update folder

--- digitaltwin_ws/M3Pro_ws/src/largemodel/largemodel/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append("/root/M3Pro_ws/src/largemodel/utils")
-
-
--- digitaltwin_ws/M3Pro_ws/src/largemodel/utils/test.py
+++
@@ -1,3 +0,0 @@
-from large_model_interface import rec_wav_music_en,Xinghou_speaktts
-
-Xinghou_speaktts("I think you are very interesting")
